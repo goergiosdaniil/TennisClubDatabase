1550fdc03ec3d3684af5900e974152a4fede2ca8
goergiosdaniil <goergios2@gmail.com>
2020-12-30 20:44:37 +0200

    Menu & Insert & Select

--- tennisClub.py
+++ tennisClub.py
@@ -14,34 +14,70 @@ password_db=os.environ.get("PASSWORD_DB")
 database=os.environ.get("DATABASE_DB")
 
 
-try:
+curs = ''
+con = ''
 
-    con = mysql.connector.connect(host=host_db,
-                                  user=user_db, password=password_db, database=database)
+def connect_to_db(): #Για να συνεδέεται στη βάση μας
+    global curs,con
+    try:
+        con = mysql.connector.connect(host=host_db,user=user_db, password=password_db, database=database)
+        curs = con.cursor()
+        print("Επιτυχής σύνδεση στην βάση δεδομένων!\n")
+    except:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+        exit()
 
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-    curs=con.cursor()
-    curs.execute("INSERT INTO `gipedo` (`Id`, `Eidos`, `Onoma`, `Texn_Diathesimotita`) VALUES (NULL, 'test', 'okok', '2')")
 
+
+def insert_gipedo(): #prosthiki ylikou
+    global curs,con
+    while (True):
+        try:
+            gipedo_onoma= input('Πως λέγεται το γήπεδο;\n')
+            gipedo_eidos = input('Τι είδους είναι το γήπεδο;\n')
+            gipedo_texn_diath = input('To γήπεδο είναι τεχν διαθέσιμο 0 ή 1 :\n')
+            insert = "INSERT INTO gipedo(Id,Eidos,Onoma,Texn_Diathesimotita) VALUES(NULL,'"+gipedo_eidos+"','"+gipedo_onoma+"','"+gipedo_texn_diath+"');"
+            curs.execute(insert)
+            con.commit()
+            ans = input('Θες να προσθέσουμε κάποιο άλλο γήπεδο; Αν ναι πατήστε 1, αλλιώς πατήστε κενό και μετά enter για επιστροφή στο αρχικό μενού!\n')
+            if (ans!='1'):
+                print('Εγινε η προσθήκη')
+                return
+        except:
+            print("Αδυναμία προσθήκης γηπέδου!")
+            return
+
+def view_gipedo(): # Αυτό απλώς εκτυπώνει τα γήπεδα πρέπει να γίνει πιο όμορφη παρουσιασή του πχ σαν πίνακα
+    global curs,con
+    print("Αυτά είναι τα γήπεδα μας")
     curs.execute("SELECT * FROM `gipedo`")
     result=curs.fetchall()
 
     for i in result:
         print(i)
 
-    con.commit()
+def menu(): #Σε αυτό το μενού πρέπει να σχεδιάσουμε τις επιλογές
+    print('Καλησπέρα!\n')
+    while True:
+        ans =input('Για να προσθέσετε γήπεδο πατήστε....1\nΓια να δείτε όλα τα γήπεδα μας πατήστε 2\nΓια έξοδο πατήστε κενό και μετά enter\n')
+        if ans=='1':
+            insert_gipedo()
+        if ans=='2':
+            view_gipedo()
+        if ans==' ':
+            return
+
+def main():#Εδώ μέσα βάζεις όποια συνάρτηση θέλεις να γίνει.
+    connect_to_db()
+    menu()
     con.close()
 
 
 
-
-
-
-
+if __name__=="__main__":
+    main()
