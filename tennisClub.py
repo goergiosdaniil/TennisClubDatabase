7148be0a76985226ed4ca8cb51f83de62f269f28
unknown <penelopegeorgiou98@gmail.com>
2020-12-31 15:33:30 +0200

    Added insert_atomo()

--- tennisClub.py
+++ tennisClub.py
@@ -52,6 +52,40 @@ def insert_gipedo(): #prosthiki gipedoy
             print("Αδυναμία προσθήκης γηπέδου!")
             return
 
+def insert_atomo(): #prosthiki ylikou
+    global curs,con
+    while (True):
+        try:
+            choice = input('Πατήστε 1 για εισαγωγή παίκτη, 2 για εισαγωγή προπονητή\n')
+            if choice!='1' and choice!='2':
+                print('Άκυρη επιλογή!')
+            else:                
+                atomo_AMKA,atomo_eponimo,atomo_onoma = input('Να γίνει εισαγωγή βασικών στοιχείων με την ακόλουθη μορφή: ΑΜΚΑ Επόνυμο Όνομα\n').split()            
+                atomo_tilefono,atomo_email=input('Να γίνει εισαγωγή στοιχείων επικοινωνίας με την ακόλουθη μορφή: Τηλέφωνο Email\n').split()
+                atomo_odos=input('Να γίνει εισαγωγή της Οδός Κατοικείας του ατόμου\n')
+                atomo_arithmos,atomo_poli,atomo_TK=input('Να γίνει εισαγωγή στοιχείων της υπόλοιπης διεύθυνσης με την ακόλουθη μορφή:Αριθμός Πόλη και TK\n').split()
+                insert1 = "INSERT INTO atomo(AMKA,Eponimo,Onoma,Tilefono,Email,Odos,Arithmos,Poli,TK) VALUES('"+atomo_AMKA+"','"+atomo_eponimo+"','"+atomo_onoma+"','"+atomo_tilefono+"','"+atomo_email+"','"+atomo_odos+"','"+atomo_arithmos+"','"+atomo_poli+"','"+atomo_TK+"');"
+                if choice=='1':
+                    paiktis_im = input('Να γίνει εισαγωγή της ημερομηνίας λήξης του δελτίου ηγείας σε μορφή YYYY-MM-DD:\n')
+                    insert2 = "INSERT INTO paiktis(AMKA,Hm_Lixis_Deltiou) VALUES('"+atomo_AMKA+"','"+paiktis_im+"');"
+                elif choice=='2':
+                    prop_AFM = input('Να γίνει εισαγωγή του ΑΦΜ του εργαζόμενου:\n')
+                    prop_wage = input('Να γίνει εισαγωγή του ορομισθίου του εργαζόμενου:\n')
+                    prop_bio = input('Να γίνει εισαγωγή του βιογραφικού του εργαζόμενου:\n')
+                    insert2 = "INSERT INTO proponitis(AMKA,AFM,Oromisthio,Viografiko) VALUES('"+atomo_AMKA+"','"+prop_AFM+"','"+prop_wage+"','"+prop_bio+"');"
+                curs.execute(insert1)
+                curs.execute(insert2)
+                con.commit()
+            ans = input('Θες να προσθέσουμε κάποιο άλλο άτομο; Αν ναι πατήστε 1, αλλιώς πατήστε κενό και μετά enter για επιστροφή στο αρχικό μενού!\n')
+            if (ans!='1'):
+                print('Εγινε η προσθήκη')
+                return
+        except:
+            print("Αδυναμία προσθήκης ατόμου.")
+            return
+
+        
+
 def kratisi():
     while True:
         ans =input('Για να κάνει κράτηση ένα άτομο πατήστε 1\nΓια έξοδο πατήστε κενό και μετά enter\n')#Μετά να δούμε αν θέλουμε και άλλα εδώ
@@ -93,11 +127,13 @@ def view_gipedo(): # Αυτό απλώς εκτυπώνει τα γήπεδα π
 def menu(): #Σε αυτό το μενού πρέπει να σχεδιάσουμε τις επιλογές
     print('Καλησπέρα!\n')
     while True:
-        ans =input('Για να προσθέσετε γήπεδο πατήστε....1\nΓια να δείτε όλα τα γήπεδα μας πατήστε 2\nΓια να κάνετε καινούργια κράτηση πατήστε το 4\nΓια έξοδο πατήστε κενό και μετά enter\n')
+        ans =input('Για να προσθέσετε γήπεδο πατήστε....1\nΓια να δείτε όλα τα γήπεδα μας πατήστε 2\nΓια να προσθέσετε παίκτη η προπονητή πατήστε 3\nΓια να κάνετε καινούργια κράτηση πατήστε το 4\nΓια έξοδο πατήστε κενό και μετά enter\n')
         if ans=='1':
             insert_gipedo()
         if ans=='2':
             view_gipedo()
+        if ans=='3':
+            insert_atomo()
         if ans=='4':
             kratisi()
         if ans==' ':
